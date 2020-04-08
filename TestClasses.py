e12846eb73564a12013c7108dd52faa5e623d4c7
BHornick <bhornic1@umbc.edu>
2020-04-08 11:57:35 -0400

    Added TODO comments for room object

--- TestClasses.py
+++ TestClasses.py
@@ -3,8 +3,9 @@ class Room:
         self.roomName = arg1 + " " + arg2
         self.timeSlotsCount = 28
         self.timeSlots = [0] * self.timeSlotsCount
+        self.roomCapacity = 0       #switch to arg3 at some point
     def addClassTime(self,timeSlot):
-        self.timeSlots[timeSlot]=1
+        self.timeSlots[timeSlot]=1      #switch data to new argument, of type "roomEvent"
     def removeClassTime(self,timeSlot):
         self.timeSlots[timeSlot]=0
     def getRoomName(self):
