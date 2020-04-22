4f29af069963a36dc9ca8d6a493edf9f8837b909
Brady <bro11@umbc.edu>
2020-04-22 14:31:22 -0400

    Updated Driver
    Loads from online database, has search and addroom

--- SQLdriver.py
+++ SQLdriver.py
@@ -1,8 +1,9 @@
 ###
 # Created on Apr 6,2020
+# Updated April 22 2020
 #
-# @author: Brett Hornick
-# bhornic1@umbc.edu
+# @author: Brady Ronayne
+# SHould take from online database
 ###
 from Room import Room
 from GUI_Search import GUI_Main
@@ -14,11 +15,19 @@ import psycopg2
 import psycopg2.extensions
 import os
 
+# connection = psycopg2.connect(user = "postgres",
+#                                   password = "bippy",
+#                                   host = "localhost",
+#                                   port = "5432",
+#                                   database = "447ver1")
+
 connection = psycopg2.connect(user = "postgres",
-                                  password = "bippy",
-                                  host = "localhost",
+                                  password = "software447",
+                                  host = "database447.cst3jimtz2ge.us-east-2.rds.amazonaws.com",
                                   port = "5432",
-                                  database = "447ver1")
+                                  database = "ClassScheduler")
+
+                                  
 #setup database
 connection.autocommit = True
 cursor = connection.cursor()
@@ -42,8 +51,18 @@ window1 = GUI_Main(interface,roomList)
 
 testBuilding = input("Class Name: ")
 
-query1 = "select building, roomnum from rooms where building = '" + window1.entry_search_building.get() + "' AND roomnum = '" + window1.entry_search_num.get() + "'"
-cursor.execute(query1)
+if(window1.entry_addRoom_name.get() != ""):
+
+    #query1 = "select building, roomnum from rooms where building = '" + window1.entry_search_building.get() + "' AND roomnum = '" + window1.entry_search_num.get() + "'"
+    query1 = "INSERT INTO rooms VALUES(7, '"+ str(window1.entry_addRoom_name.get()) + "', '" + str(window1.entry_addRoom_number.get()) + "', 30, 'Classroom') ON CONFLICT DO NOTHING;"
+    cursor.execute(query1)
+
+if(window1.entry_search_building.get() != ""):
+    query1 = "Select building, roomnum from rooms where building = '" + str(window1.entry_search_building.get())+"' and roomnum = '"+ str(window1.entry_search_num.get()) + "';"
+    cursor.execute(query1)
+else:
+    query1 = "select building, roomnum from rooms"
+    cursor.execute(query1)
 
 roomList = []
 for building, roomnum in cursor.fetchall():
