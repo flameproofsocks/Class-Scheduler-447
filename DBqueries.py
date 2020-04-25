bfc0ab6e7ca735da7adc75a3cb61d87cd140dd83
Brady <bro11@umbc.edu>
2020-04-24 22:00:42 -0400

    Added UI to pull from database
    Created DBqueries (A set of queries in an easy to execute form)
    Added the events to be displayed in each quadrant of the UI.

---
+++ DBqueries.py
@@ -0,0 +1,86 @@
+queryClearEvents = """
+DROP TABLE IF EXISTS classes CASCADE ;
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE events (
+	resID integer NOT NULL,
+	eventName varchar(30),
+	courseNum varchar(30), --this can be a string, might be “100L”
+	days int, -- days of the week as binary - 10101 (mwf) 
+	startTime time,
+	endTime time,
+	classInfo text,
+	roomID integer NOT NULL,
+	profID integer,
+	FOREIGN KEY (roomID) REFERENCES rooms (roomID) ON DELETE CASCADE,
+	FOREIGN KEY (profID) REFERENCES prof (profID) ON DELETE CASCADE,
+	PRIMARY KEY (resID)
+);
+"""
+
+queryLoadEvents = """
+INSERT INTO events VALUES(1, 'Bio', '101', 10100, '10:00:00', '11:15:00', 'Generic Class', 1, 1) ON CONFLICT DO NOTHING;
+
+INSERT INTO events VALUES(2, 'CMSC', '447', 10100, '10:00:00', '11:15:00', 'Generic Class', 1, 1) ON CONFLICT DO NOTHING;
+
+INSERT INTO events VALUES(3, 'Math', '112L', 10100, '10:00:00', '11:15:00', 'Generic Class', 4, 1) ON CONFLICT DO NOTHING;
+
+INSERT INTO events VALUES(4, 'Bio', '201', 10100, '10:00:00', '11:15:00', 'Generic Class', 3, 1) ON CONFLICT DO NOTHING;
+
+INSERT INTO events VALUES(5, 'Bio', '301', 10100, '10:00:00', '11:15:00', 'Generic Class', 3, 1) ON CONFLICT DO NOTHING;
+"""
+
+queryClearAll = """
+DROP TABLE IF EXISTS rooms CASCADE;
+CREATE TABLE rooms (
+	roomID integer NOT NULL,
+	building text,
+	roomNum integer,
+	capacity integer,
+	roomType varchar(30),
+	PRIMARY KEY (roomID)
+);
+
+DROP TABLE IF EXISTS prof CASCADE ;
+CREATE TABLE prof (
+	profID integer NOT NULL,
+	fName varchar(40),
+	lName varchar(40),
+	allergies varchar(40),
+	website varchar(40),
+	information text,
+	PRIMARY KEY (profID)
+);
+
+DROP TABLE IF EXISTS classes CASCADE ;
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE events (
+	resID integer NOT NULL,
+	eventName varchar(30),
+	courseNum varchar(30), --this can be a string, might be “100L”
+	days int, -- days of the week as binary - 10101 (mwf) 
+	startTime time,
+	endTime time,
+	classInfo text,
+	roomID integer NOT NULL,
+	profID integer,
+	FOREIGN KEY (roomID) REFERENCES rooms (roomID) ON DELETE CASCADE,
+	FOREIGN KEY (profID) REFERENCES prof (profID) ON DELETE CASCADE,
+	PRIMARY KEY (resID)
+);
+
+"""
+
+queryLoadRooms = """
+INSERT INTO rooms VALUES(1, 'ILSB Building', 118, 30, 'Classroom') ON CONFLICT DO NOTHING;
+INSERT INTO rooms VALUES(2, 'Bio', 201, 30, 'Classroom') ON CONFLICT DO NOTHING;
+
+INSERT INTO rooms VALUES(3, 'Bio', 202, 30, 'Classroom') ON CONFLICT DO NOTHING;
+
+INSERT INTO rooms VALUES(4, 'Math', 201, 30, 'Classroom') ON CONFLICT DO NOTHING;
+
+INSERT INTO rooms VALUES(5, 'Math', 202, 30, 'Classroom') ON CONFLICT DO NOTHING;
+
+INSERT INTO rooms VALUES(6, 'Math', 301, 30, 'Classroom') ON CONFLICT DO NOTHING;
+
+
+"""
--- SQLdriver.py
+++ SQLdriver.py
@@ -6,10 +6,12 @@
 # SHould take from online database
 ###
 from Room import Room
-from GUI_Search import GUI_Main
+from Events import Events
+from GUI_Main import GUI_Main
 from tkinter import *
 from tkinter.font import Font
 from PIL import Image, ImageTk
+import DBqueries
 
 import psycopg2
 import psycopg2.extensions
@@ -37,12 +39,42 @@ cursor.execute("SELECT version();")
 record = cursor.fetchone()
 print("You are connected to - ", record,"\n")
 
-query1 = "select building, roomnum, capacity from rooms"
+###reload events
+# cursor.execute(DBqueries.queryClearAll)
+# cursor.execute(DBqueries.queryLoadRooms)
+# cursor.execute("INSERT INTO prof VALUES(1, 'John', 'Smith', 'Chalk Allergy', 'www.website.com', 'A generic teacher, hates chalk, talks loudly, students still fall asleep' ) ON CONFLICT DO NOTHING;")
+# cursor.execute(DBqueries.queryLoadEvents)
+
+query1 = "select roomid, building, roomnum, capacity from rooms"
 cursor.execute(query1)
 
 roomList = []
-for building, roomnum, capacity in cursor.fetchall():
+idList = []
+for roomid, building, roomnum, capacity in cursor.fetchall():
     roomList.append(Room(building, str(roomnum) , str(capacity)) )
+    idList.append(roomid)
+
+
+# self.subject = arg1
+# self.courseNum = arg2
+# self.version = arg3
+# self.section = arg4
+# self.instructor = arg5
+# self.time = arg6
+# self.capacity = arg7
+#event1 = Events("CMSC","100"," ","01","Professor X","MW1",200)
+
+#Code to add events within the room
+j = 1
+cursor2 = connection.cursor()
+for i in range(len(roomList)):
+    query1 = "Select resID, eventName, courseNum, profID from events where roomID = " + str(idList[i]) 
+    cursor2.execute(query1)
+    for resID, eventName, courseNum, profID in cursor2.fetchall():
+        roomList[i].addEvent(j,Events(eventName, courseNum," ","02",profID,"TTH1",50))
+        j += 1 #index for adding events
+    
+
 
 interface = Tk()
 interface.geometry("800x600")
