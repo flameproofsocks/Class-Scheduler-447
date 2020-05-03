880a2cdf60730553cf4fb84214b95af1571c4897
dgasnik1 <61060328+dgasnik1@users.noreply.github.com>
2020-05-03 13:34:54 -0400

    Update driver.py
    Add a few rooms to test. 
    Add the testNameOfUser option to add in the name of who is responsible for the reservation (student or T.A can also do this instead of using professors name for flexibility). 
    Add the testTimeNeeded option for a professor to indicate how much time they need for a room to be in use.

--- driver.py
+++ driver.py
@@ -17,22 +17,26 @@ room2 = Room("BIO","102",100)
 room3 = Room("MATH","101",50)
 room4 = Room("MATH","201",25)
 room5 = Room("ENG","101",25)
+room6 = Room("CMSC", "103", 30)
+room7 = Room("CMSC", "202", 40)
 event1 = Events("CMSC","100"," ","01","Professor X","MW1",200)
 
 
 room5.addEvent(1,event1)
 event2 = Events("CMSC","447"," ","01","Kartchner","MW10",30)
 room5.addEvent(27,event2)
-roomList = [room1,room2,room3,room4,room5]
+roomList = [room1,room2,room3,room4,room5, room6, room7]
 
 interface = Tk()
 interface.geometry("800x600")
 interface.configure(bg="black")
 window1 = GUI_Main(interface,roomList)
 
+testNameOfUser = input("Your Name: ")
 testBuilding = input("Building Name: ")
 testRNumber = input("Room Number: ")
 testCapacity = input("Room Capacity: ")
+testTimeNeeded = input("Time Required in Minutes: ")
 room6 = Room(testBuilding,testRNumber,testCapacity)
 roomList.append(room6)
 window1.buildItems(roomList,0,0)
