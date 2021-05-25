f1aee0b974a9dde40abd0b281b12c16915df650c
Brady Ronayne <46193736+flameproofsocks@users.noreply.github.com>
2021-05-24 22:07:11 -0400

    Update fileOperations.py

--- fileOperations.py
+++ fileOperations.py
@@ -1,13 +1,5 @@
-# from tkinter import filedialog
-
-# def selectFile(self,fileName):
-#         fileName = filedialog.askopenfilename()
-
-# def addFile(self,fileName,fileType):
-#         f = open(fileName,'r')
-#         fType = fileType;       #0 for room list, 1 for event list,see examples provided for format
-
-#         #parse data into proper fields, send to database
+# Author:
+# Ahmed
 
 from tkinter import filedialog
 import DBqueries
